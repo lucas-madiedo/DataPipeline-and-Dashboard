82907f3e5282d3f79f4cb970ea24af4f68154f53
Lucas Madiedo <lmadiedoriera@gmail.com>
2020-06-27 14:55:01 +0200

    main_script file created

--- main_script.py
+++ main_script.py
@@ -1,26 +1,29 @@
 import argparse
-from p_acquisition import m_acquisition as mac
-from p_wrangling import m_wrangling as mwr
-from p_analysis import m_analysis as man 
-from p_reporting import m_reporting as mre 
+from p_acquisition import m_acquisition
+from p_wrangling import m_wrangling
+
 
 def argument_parser():
-    parser = argparse.ArgumentParser(description = 'Set chart type')
-    parser.add_argument("-b", "--bar", help="Produce a barplot", action="store_true")
-    parser.add_argument("-l", "--line", help="Produce a lineplot", action="store_true")
+    parser = argparse.ArgumentParser(description='Specify the path to the darta')
+    parser.add_argument("-p","--path", type=str, help='specify companies list flies', required=True)
     args = parser.parse_args()
     return args
 
-def main(some_args):
-    data = mac.acquire()
-    filtered = mwr.wrangle(data, year)
-    results = man.analyze(filtered)
-    fig = mre.plotting_function(results, title, arguments)
-    mre.save_viz(fig, title)
-    print('========================= Pipeline is complete. You may find the results in the folder ./data/results =========================')
+def main(args):
+    print('starting Pipelie')
 
-if __name__ == '__main__':
-    year = int(input('Enter the year: '))
-    title = 'Top 10 Manufacturers by Fuel Efficiency ' + str(year)
+    conection = m_acquisition.create_sql_conexion(args.path)
+    m_acquisition.clean_personal_db(conection)
+    m_acquisition.clean_poll_df(conection)
+    m_acquisition.countries_info_extract(conection)
+    m_acquisition.extract_carrer_info(conection)
+
+    m_wrangling.create_full_raw_table()
+
+
+
+    print('========================= Pipeline is complete =========================')
+
+if __name__ == "__main__":
     arguments = argument_parser()
     main(arguments)
